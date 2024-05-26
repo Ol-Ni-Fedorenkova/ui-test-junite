6de6d2473d1856b7d212725f4b0a274db56165c9
Olga Fedorenkova <fedorenkovaon@yandex.ru>
2024-05-26 20:41:48 +0300

    настроила запуск в selenoid

--- src/test/java/example/utils/DriverManager.java
+++ src/test/java/example/utils/DriverManager.java
@@ -3,6 +3,11 @@ package example.utils;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class DriverManager {
     private static final ThreadLocal<WebDriver> DRIVER = new ThreadLocal<>();
@@ -11,9 +16,20 @@ public class DriverManager {
     }
 
     public static WebDriver getDriver() {
+        String remoteUrl = "http://192.168.1.9:4444/wd/hub";
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        options.addArguments("window-size=1920x1080");
+        options.setCapability("browserVersion", "125.0");
+//        options.setCapability("enableVNC", "false");
+//        options.setCapability("selenoid:options", "Test");
         if (DRIVER.get() == null) {
-            WebDriverManager.chromedriver().setup();
-            DRIVER.set(new ChromeDriver());
+//            WebDriverManager.chromedriver().setup();
+            try {
+                DRIVER.set(new RemoteWebDriver(new URL(remoteUrl), options));
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
         }
         return DRIVER.get();
     }
